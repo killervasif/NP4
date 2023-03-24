29305f78456fbcf313933cbdfe6b05a3d60bbea1
vasif <vasifbabazade2004@gmail.com>
2023-03-24 14:21:50 +0400

    Add project files.

---
+++ NP4/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NP4
+{
+    
+    public partial class MainWindow : Window
+    {
+        private UdpClient client;
+        private IPEndPoint connectEP;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            client = new UdpClient();
+            connectEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 45678);
+        }
+
+        private static BitmapImage? LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var buffer=new byte[ushort.MaxValue-29];
+            await client.SendAsync(buffer, buffer.Length, connectEP);
+            var len = 0;
+            var limit=buffer.Length;
+            var list=new List<byte>();
+            while (true)
+            {
+                do
+                {
+                    var result=await client.ReceiveAsync();
+                    buffer=result.Buffer;
+                    len = buffer.Length;
+                    list.AddRange(buffer);
+                } while(len==limit);
+                Img.Source = LoadImage(list.ToArray());
+                list.Clear();
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Drawing;
+
+UdpClient server = new UdpClient(45678);
+
+while (true)
+{
+    var result = await server.ReceiveAsync();
+
+    new Task(async () =>
+    {
+
+        var remoteEP = result.RemoteEndPoint;
+        while (true)
+        {
+            await Task.Delay(20);
+            var img = ScreenShot();
+
+            var bytesImg = ImageToByte(img);
+
+
+            var myArray = bytesImg.Chunk(ushort.MaxValue - 29);
+
+            foreach (var array in myArray)
+                await server.SendAsync(array, array.Length, remoteEP);
+        }
+
+    }).Start();
+}
+
+byte[] ImageToByte(Image img)
+{
+    using(var stream = new MemoryStream())
+    {
+        img.Save(stream,System.Drawing.Imaging.ImageFormat.Jpeg);
+        return stream.ToArray();
+    }
+}
+
+Image ScreenShot()
+{
+    Bitmap memo;
+    var width = 1920;
+    var height = 1080;
+    memo= new Bitmap(width, height); 
+    Graphics g = Graphics.FromImage(memo);
+    g.CopyFromScreen(0, 0, 0, 0, memo.Size);
+    return memo;
+}
